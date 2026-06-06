05f12d128457e1d96e06cfa27b5c0b4f42121f77
Zainal <zaenalarifin312006@gmail.com>
2026-06-06 23:05:33 +0700

    update landing page & logic js

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const ctaFeedback = document.querySelector("#cta-feedback");
 let counter = 0;
 
 ctaBtn.addEventListener("click", () => {
-  counter++;
-  console.log(counter);
+	counter++;
+	console.log("Jumlah klik:", counter);
 
-  ctaFeedback.classList.remove("hidden");
-  ctaFeedback.textContent = "Halo harusnya muncul";
+	if (counter === 1) {
+		ctaFeedback.classList.remove("hidden");
+		ctaFeedback.textContent = "Halo harusnya muncul";
+	} else if (counter === 2) {
+		ctaFeedback.textContent = "Kamu sudah klik sebanyak 2 kali!";
+	} else if (counter > 2) {
+		ctaBtn.disabled = true;
+		ctaFeedback.textContent = "Kamu sudah mencapai batas klik!";
+	}
 });
